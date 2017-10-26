52066f198e58ce10d388f3508471b365e1cf007d
assassin <assassin_cike@163.com>
2017-10-27 00:25:38 +0800

    remove d.ts file where under typings dir

--- typings/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="globals/jest/index.d.ts" />
-/// <reference path="globals/object-assign/index.d.ts" />
-/// <reference path="globals/react-router-dom/index.d.ts" />
-/// <reference path="globals/redux-saga/index.d.ts" />
-/// <reference path="globals/redux/index.d.ts" />
-/// <reference path="globals/webpack-dev-server/index.d.ts" />
-/// <reference path="globals/whatwg-fetch/index.d.ts" />

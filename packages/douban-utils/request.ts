ac948c8f7d981999dc4c9c8f33a6d6787a77d316
assassin <assassin_cike@163.com>
2017-10-29 22:28:32 +0800

    provide redux to index view

---
+++ packages/douban-utils/request.ts
@@ -0,0 +1,11 @@
+const baseUrl = 'https://api.douban.com/v2';
+
+export default function(url: string, methods: string) {
+  return fetch(baseUrl + url)
+    .then(res => {
+      return res.json();
+    })
+    .catch(exp => {
+      console.warn(exp);
+    });
+}
--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,20 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import 'babel-polyfill';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import reducer from './reducers';
+const store = createStore(reducer);
 
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+    <Router>
+      <Route path="/" component={App} />
+    </Router>
+  </Provider>,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
